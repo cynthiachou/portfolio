320b374ff884027d65881b2b7a549a555466ab1d
Yihan Zhou <zhanonly@gmail.com>
2019-04-04 11:25:37 -0700

    substring in string

--- 03_dragAndDropFile/sketch.js
+++ 03_dragAndDropFile/sketch.js
@@ -1,26 +1,26 @@
 var dropzone;
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(windowWidth, windowHeight);
   background(0);
 
   dropzone = select('#dropzone');
   dropzone.dragOver(highlight);
   dropzone.dragLeave(unhighlight);
-  dropzone.drop(gotFile,unhighlight);
+  dropzone.drop(gotFile, unhighlight);
 }
 
-function highlight(){
-  dropzone.style('background-color','#ccc');
+function highlight() {
+  dropzone.style('background-color', '#ccc');
 }
 
-function unhighlight(){
-  dropzone.style('background-color','#fff');
+function unhighlight() {
+  dropzone.style('background-color', '#fff');
 }
 
-function gotFile(file){
+function gotFile(file) {
   var img;
-  img=createImg(file.data);
-  img.size(100,100);
-  image(img,0,0,200,200)
+  img = createImg(file.data);
+  img.size(100, 100);
+  image(img, 0, 0, 200, 200)
 }
